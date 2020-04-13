3b0e7e08d78358a7e365ed8a14d5699b1d803e8e
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-04-13 13:15:18 -0600

    completed html and created a style.css

--- bootstrap-challenge.php
+++ bootstrap-challenge.php
@@ -5,11 +5,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>bootsrap!</title>
+	<link rel="stylesheet" href="style/style.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </head>
-<!-- Bootstrap CSS -->
 <body>
 <div class="container mt-3">
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -22,23 +21,21 @@
 				<li class="nav-item active">
 					<a class="nav-link" href="#">Home <span class="sr-only"></span></a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item active">
 					<a class="nav-link" href="#">Gadgets</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item active">
 					<a class="nav-link" href="#">Pricing</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item active">
 					<a class="nav-link" href="#">About</a>
 				</li>
 			</ul>
 		</div>
 	</nav>
 </div>
-<br>
-<br>
-<div class = 'container'>
 
+<div class = 'container space'>
 	<div class = 'jumbotron'>
 		<h1>Hello, capt</h1>
 		<p>I’m not here to frack spiders, au, you don’t know his story, bro.
@@ -51,71 +48,44 @@
 		<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 	</div>
 </div>
-
 <div class="container-fluid mt-5" style="background-color: #ddd">
 	<div class="container">
 		<div class="row pt-5 pb-5">
-			<div class="col-md-3"><img src="" alt="some-photo of captain gadget"></div>
+			<div class="col-md-3 centered" >
+				<img src="image/gogo.jpg" alt="some-photo of captain gadget" width="200" height="200">
+			</div>
 			<div class="col-md-9">	<h5 class="mt-0">Media heading</h5>
 				Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</div>
 		</div>
 	</div>
 </div>
-
 <div class="container pt-5 pb-5">
 	<div class="row">
-
 		<div class="col-md-3 p-5 centered">
-
 			<img class="round" src="https://via.placeholder.com/200" alt="some-photo of captain gadget">
-
 			<div class="mt-5 p-3 text-box">
-
 				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt </p>
-
 			</div>
-
 		</div>
-
-
-
 		<div class="col-md-3 p-5 centered" >
-
 			<img class="round" src="https://via.placeholder.com/200" alt="some-photo of captain gadget">
 			<div class="mt-5 p-3 text-box">
-
-				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt </p>
-
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt distinctio t</p>
 			</div>
-
 		</div>
-
-
-
 		<div class="col-md-3 p-5 centered">
-
 			<img class="round" src="https://via.placeholder.com/200" alt="some-photo of captain gadget">
 			<div class="mt-5 p-3 text-box">
-
-				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt </p>
-
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt distinctio </p>
 			</div>
-
 		</div>
-
-
 		<div class="col-md-3 p-5 centered">
-
 			<img class="round" src="https://via.placeholder.com/200" alt="some-photo of captain gadget"> <div class="mt-5 p-3 text-box">
-
-				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt </p>
-
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, rerum quo aut distinctio recusandae quaerat! Nesciunt distinctio </p>
 			</div>
 		</div>
 	</div>
 </div>
-
-
 <footer class="container-fluid p-3">
 	<p>lorem something</p>
 </footer>
